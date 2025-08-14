3233275e646ea9731fdfcc2d5fbe337bc2dd8b9b
itxworks <73403258+itxworks@users.noreply.github.com>
2025-08-14 16:31:59 +0200

    Update entrypoint.sh
    check for script exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# Start the Xvfb server with specified screen size and depth, then run the Python script
 xvfb-run --auto-servernum --server-args="-screen 0 1920x1080x16" python3 check_switch.py
+
+if [ ! -f /app/check_switch.py ]; then
+    echo "ERROR: /app/check_switch.py not found!"
+    ls -l /app
+    exit 1
+fi
+
+# Start the Xvfb server with specified screen size and depth, then run the Python script
+xvfb-run --auto-servernum --server-args="-screen 0 1920x1080x16" python3 /app/check_switch.py
 
 # Exec the given command (allows passing arguments when running the container)
 exec "$@"
